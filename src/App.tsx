c20663d53698c801b236e4b287c826a5a60d4661
Guillermo Rodriguez <guillermo.rodriguez@bentley.com>
2022-07-06 13:28:34 -0700

    crappy imodel selection

--- src/App.tsx
+++ src/App.tsx
@@ -3,158 +3,110 @@
  * See LICENSE.md in the project root for license terms and full copyright notice.
  *--------------------------------------------------------------------------------------------*/
 
-import './App.scss';
+import "./App.scss";
+import { PageLayout } from "@itwin/itwinui-layouts-react";
+import "@itwin/itwinui-layouts-css/dist/styles.css";
+import { BrowserAuthorizationClient } from "@itwin/browser-authorization";
+import type { ScreenViewport } from "@itwin/core-frontend";
+import { FitViewTool, IModelApp, StandardViewId } from "@itwin/core-frontend";
+import { useAccessToken, Viewer } from "@itwin/web-viewer-react";
+import React, { useCallback, useEffect, useMemo, useState } from "react";
+import { SvgMoon, SvgSun } from "@itwin/itwinui-icons-react";
+import { GroupingMappingProvider } from "@itwin/grouping-mapping-widget";
+import {
+  ReportsConfigProvider,
+  ReportsConfigWidget,
+  REPORTS_CONFIG_BASE_URL,
+} from "@itwin/reports-config-widget-react";
+import { Button, IconButton, ThemeType, useTheme } from "@itwin/itwinui-react";
+import { useSearchParams } from "react-router-dom";
+import SelectIModel from "./SelectIModel";
+import SelectProject from "./SelectProject";
+import { IModelFull, ProjectFull } from "@itwin/imodel-browser-react";
+import { SampleViewer } from "./Viewer";
+import { AccessToken } from "@itwin/core-bentley";
+import AuthClient from "./AuthClient";
 
-import { BrowserAuthorizationClient } from '@itwin/browser-authorization';
-import type { ScreenViewport } from '@itwin/core-frontend';
-import { FitViewTool, IModelApp, StandardViewId } from '@itwin/core-frontend';
-import { useAccessToken, Viewer } from '@itwin/web-viewer-react';
-import React, { useCallback, useEffect, useMemo, useState } from 'react';
-import { SvgMoon, SvgSun } from '@itwin/itwinui-icons-react';
-import { GroupingMappingProvider } from '@itwin/grouping-mapping-widget';
-
-import { history } from './history';
-import { Button, IconButton, ThemeType, useTheme } from '@itwin/itwinui-react';
+export const prefixUrl = (baseUrl?: string, prefix?: string) => {
+  if (prefix && baseUrl) {
+    return baseUrl.replace("api.bentley.com", `${prefix}api.bentley.com`);
+  }
+  return baseUrl;
+};
 
 const App: React.FC = () => {
-  const [iModelId, setIModelId] = useState(process.env.IMJS_IMODEL_ID);
-  const [iTwinId, setITwinId] = useState(process.env.IMJS_ITWIN_ID);
-
-  const accessToken = useAccessToken();
-
-  const authClient = useMemo(
-    () =>
-      new BrowserAuthorizationClient({
-        scope: process.env.IMJS_AUTH_CLIENT_SCOPES ?? '',
-        clientId: process.env.IMJS_AUTH_CLIENT_CLIENT_ID ?? '',
-        redirectUri: process.env.IMJS_AUTH_CLIENT_REDIRECT_URI ?? '',
-        postSignoutRedirectUri: process.env.IMJS_AUTH_CLIENT_LOGOUT_URI,
-        responseType: 'code',
-        authority: process.env.IMJS_AUTH_AUTHORITY,
-      }),
-    []
+  const [searchParams, setSearchParams] = useSearchParams();
+  const [accessToken, setAccessToken] = useState<AccessToken>();
+  const [iModelId, setIModelId] = useState<string>(
+    searchParams.get("iModelId") ?? ""
+  );
+  const [iTwinId, setITwinId] = useState<string>(
+    searchParams.get("iTwinId") ?? ""
   );
 
-  const login = useCallback(async () => {
-    try {
-      await authClient.signInSilent();
-    } catch {
-      await authClient.signIn();
-    }
-  }, [authClient]);
-
   useEffect(() => {
-    void login();
-  }, [login]);
+    const initAuth = async () => {
+      if (!AuthClient.client) {
+        const client = AuthClient.initialize();
 
-  useEffect(() => {
-    if (accessToken) {
-      const urlParams = new URLSearchParams(window.location.search);
-      if (urlParams.has('iTwinId')) {
-        setITwinId(urlParams.get('iTwinId') as string);
-      } else {
-        if (!process.env.IMJS_ITWIN_ID) {
-          throw new Error(
-            'Please add a valid iTwin ID in the .env file and restart the application or add it to the iTwinId query parameter in the url and refresh the page. See the README for more information.'
-          );
+        try {
+          // attempt silent signin
+          await AuthClient.signInSilent();
+        } catch (error) {
+          // if silent sign in fails, have user manually sign in
+          await AuthClient.signIn();
         }
+
+        setAccessToken(await client.getAccessToken());
       }
-
-      if (urlParams.has('iModelId')) {
-        setIModelId(urlParams.get('iModelId') as string);
-      } else {
-        if (!process.env.IMJS_IMODEL_ID) {
-          throw new Error(
-            'Please add a valid iModel ID in the .env file and restart the application or add it to the iModelId query parameter in the url and refresh the page. See the README for more information.'
-          );
-        }
-      }
-    }
-  }, [accessToken]);
-
-  useEffect(() => {
-    if (accessToken && iTwinId && iModelId) {
-      history.push(`?iTwinId=${iTwinId}&iModelId=${iModelId}`);
-    }
-  }, [accessToken, iTwinId, iModelId]);
-
-  const [theme, setTheme] = React.useState<ThemeType>(
-    (localStorage.getItem('THEME') as ThemeType) ?? 'light'
-  );
-  useTheme(theme);
-  React.useEffect(() => {
-    document.documentElement.setAttribute('data-theme', theme);
-    localStorage.setItem('THEME', theme);
-  }, [theme]);
-
-  /** NOTE: This function will execute the "Fit View" tool after the iModel is loaded into the Viewer.
-   * This will provide an "optimal" view of the model. However, it will override any default views that are
-   * stored in the iModel. Delete this function and the prop that it is passed to if you prefer
-   * to honor default views when they are present instead (the Viewer will still apply a similar function to iModels that do not have a default view).
-   */
-  const viewConfiguration = useCallback((viewPort: ScreenViewport) => {
-    // default execute the fitview tool and use the iso standard view after tile trees are loaded
-    const tileTreesLoaded = () => {
-      return new Promise((resolve, reject) => {
-        const start = new Date();
-        const intvl = setInterval(() => {
-          if (viewPort.areAllTileTreesLoaded) {
-            clearInterval(intvl);
-            resolve(true);
-          }
-          const now = new Date();
-          // after 20 seconds, stop waiting and fit the view
-          if (now.getTime() - start.getTime() > 20000) {
-            reject();
-          }
-        }, 100);
-      });
     };
 
-    tileTreesLoaded().finally(() => {
-      void IModelApp.tools.run(FitViewTool.toolId, viewPort, true, false);
-      viewPort.view.setStandardRotation(StandardViewId.Iso);
-    });
+    initAuth().catch(console.error);
+
+    return () => {
+      AuthClient.dispose();
+    };
   }, []);
 
-  const viewCreatorOptions = useMemo(
-    () => ({ viewportConfigurer: viewConfiguration }),
-    [viewConfiguration]
-  );
+  const prefix = `${process.env.IMJS_URL_PREFIX}`.slice(0, -1) as any;
+
+  const onIModelSelect = (iModel: IModelFull) => {
+    setIModelId(iModel.id);
+    searchParams.set("iModelId", iModel.id)
+    setSearchParams(searchParams);
+  };
+  const onProjectSelect = (project: ProjectFull) => {
+    setITwinId(project.id);
+    setSearchParams({ iTwinId: project.id });
+  };
 
   return (
-    <>
-      <div className='viewer-container'>
-        <div className='very-super-basic-header'>
-          <IconButton
-            key='theme'
-            styleType='borderless'
-            onClick={() =>
-              setTheme((theme) => (theme === 'light' ? 'dark' : 'light'))
-            }
-          >
-            {theme === 'light' ? <SvgMoon /> : <SvgSun />}
-          </IconButton>
-          <Button
-            onClick={async () => {
-              await authClient.signOut();
-            }}
-          >
-            Sign Out
-          </Button>
-        </div>
-
-        <Viewer
-          iTwinId={iTwinId}
-          iModelId={iModelId}
-          authClient={authClient}
-          viewCreatorOptions={viewCreatorOptions}
-          enablePerformanceMonitors={true} // see description in the README (https://www.npmjs.com/package/@itwin/desktop-viewer-react)
-          theme={theme}
-          uiProviders={[new GroupingMappingProvider()]}
-        />
-      </div>
-    </>
+    <PageLayout>
+      <PageLayout.Content>
+        {iTwinId && iModelId && AuthClient.client ? (
+          <SampleViewer
+            iTwinId={iTwinId}
+            iModelId={iModelId}
+            prefix={prefix}
+            authClient={AuthClient.client}
+          />
+        ) : iTwinId ? (
+          <SelectIModel
+            projectId={iTwinId}
+            prefix={prefix}
+            accessToken={accessToken ?? ""}
+            onSelect={onIModelSelect}
+            backFn={() => { setSearchParams({}); setITwinId("") }}
+          />
+        ) : (
+          <SelectProject
+            prefix={prefix}
+            accessToken={accessToken ?? ""}
+            onSelect={onProjectSelect}
+          />
+        )}
+      </PageLayout.Content>
+    </PageLayout>
   );
 };
 
---
+++ src/Viewer.tsx
@@ -0,0 +1,81 @@
+import { UiFramework } from "@itwin/appui-react";
+import { ScreenViewport, IModelApp, FitViewTool, StandardViewId } from "@itwin/core-frontend";
+import { GroupingMappingProvider } from "@itwin/grouping-mapping-widget";
+import { ThemeType } from "@itwin/itwinui-react";
+import { ReportsConfigWidget, ReportsConfigProvider, REPORTS_CONFIG_BASE_URL } from "@itwin/reports-config-widget-react";
+import { Viewer, ViewerAuthorizationClient } from "@itwin/web-viewer-react";
+import React from "react";
+import { useCallback, useMemo, useEffect } from "react";
+import { prefixUrl } from "./App";
+
+interface SampleViewerProps {
+  iTwinId: string,
+  iModelId: string,
+  prefix: "" | "dev" | "qa" | undefined,
+  authClient: ViewerAuthorizationClient
+}
+
+export const SampleViewer = ({ iTwinId, iModelId, authClient, prefix }: SampleViewerProps) => {
+  /** NOTE: This function will execute the "Fit View" tool after the iModel is loaded into the Viewer.
+   * This will provide an "optimal" view of the model. However, it will override any default views that are
+   * stored in the iModel. Delete this function and the prop that it is passed to if you prefer
+   * to honor default views when they are present instead (the Viewer will still apply a similar function to iModels that do not have a default view).
+   */
+  const viewConfiguration = useCallback((viewPort: ScreenViewport) => {
+    // default execute the fitview tool and use the iso standard view after tile trees are loaded
+    const tileTreesLoaded = () => {
+      return new Promise((resolve, reject) => {
+        const start = new Date();
+        const intvl = setInterval(() => {
+          if (viewPort.areAllTileTreesLoaded) {
+            clearInterval(intvl);
+            resolve(true);
+          }
+          const now = new Date();
+          // after 20 seconds, stop waiting and fit the view
+          if (now.getTime() - start.getTime() > 20000) {
+            reject();
+          }
+        }, 100);
+      });
+    };
+
+    tileTreesLoaded().finally(() => {
+      void IModelApp.tools.run(FitViewTool.toolId, viewPort, true, false);
+      viewPort.view.setStandardRotation(StandardViewId.Iso);
+    });
+  }, []);
+
+  const viewCreatorOptions = useMemo(
+    () => ({ viewportConfigurer: viewConfiguration }),
+    [viewConfiguration]
+  );
+
+  const handleOnIModelAppInit = async () => {
+    // await IModelApp.localization.changeLanguage("en-PSEUDO");
+    UiFramework.setUiVersion("1")
+  };
+
+  useEffect(() => {
+    ReportsConfigWidget.initialize(IModelApp.localization);
+  })
+  const [theme, setTheme] = React.useState<ThemeType>(
+    (localStorage.getItem('THEME') as ThemeType) ?? 'light'
+  );
+
+  return (
+    <Viewer
+      iTwinId={iTwinId}
+      iModelId={iModelId}
+      authClient={authClient}
+      viewCreatorOptions={viewCreatorOptions}
+      onIModelAppInit={handleOnIModelAppInit}
+      // additionalI18nNamespaces={["ReportsConfigWidget"]}
+      enablePerformanceMonitors={true} // see description in the README (https://www.npmjs.com/package/@itwin/desktop-viewer-react)
+      theme={theme}
+      uiProviders={[new GroupingMappingProvider({ prefix: prefix }), new ReportsConfigProvider(undefined, prefixUrl(REPORTS_CONFIG_BASE_URL, process.env.IMJS_URL_PREFIX))]} />
+  )
+
+  // new ReportsConfigProvider(undefined, prefixUrl(REPORTS_CONFIG_BASE_URL, process.env.IMJS_URL_PREFIX))
+  // new GroupingMappingProvider(undefined, `${process.env.IMJS_URL_PREFIX}`.slice(0,-1) as any)
+}
